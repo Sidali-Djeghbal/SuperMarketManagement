cbae3b68c54418366f203f19c327c065893d7d29
Sidali Djeghbal <sidalidjeghbal@gmail.com>
2025-04-21 15:26:49 +0100

    feat(ShiftManagement): add shift management component and update staff management

--- src/types/Entities.ts
+++ src/types/Entities.ts
@@ -1,24 +1,14 @@
-interface BaseEmployee {
+export interface Cashier {
   id: number;
-  name: string;
+  username: string;
   email: string;
-  phone: string;
-}
-
-export interface Worker extends BaseEmployee {
-  staffType: "worker";
-}
-
-export interface Cashier extends BaseEmployee {
-  staffType: "cashier";
-  machineNumber: number;
+  password: string;
 }
 
 export interface Machine {
   id: number;
-  machineNumber: number;
 }
 
-export type Employee = Worker | Cashier;
+export type Employee = Cashier;
 
 export type StaffItem = Employee | Machine;
